643ef6f59c7832937304dee608fc100d1ec3b644
Huỳnh Du Lượng <52560511+huynhduluong@users.noreply.github.com>
2022-09-06 06:28:07 +0700

    update stripe

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import './checkout.styles.scss';
 import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 export default function Checkout() {
     const cartItems = useSelector(selectCartItems)
@@ -30,6 +31,7 @@ export default function Checkout() {
                 <CheckoutItem key={cartItem.id} cartItem={cartItem} />
             ))}
             <div className='total'>TOTAL: ${cartTotal}</div>
+            <PaymentForm />
         </div>
     )
 }
